89b29fcb2f184e4fbbc9a8d1bb84df030a80cd86
ecemozturkk <ecemozturk704@gmail.com>
2023-03-23 00:17:26 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Ecem Öztürk on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
